061c74b72bb0b36ad32450b5995be85fb4185a65
Cristina <clmbjcm@gmail.com>
2022-10-27 23:19:33 -0300

    actions/reducer/types->brands

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,47 +3,62 @@ import {
     GET_PRODUCTS,
     PRODUCT_DETAIL,
     PRODUCT_UPDATE,
-    GET_CATEGORIES
+    GET_CATEGORIES,
+    GET_BRANDS
 } from './types';
 
-export function getProducts (){
-    return async function (dispatch){
+export function getProducts() {
+    return async function (dispatch) {
         const allData = await axios.get('/products')
-        return dispatch({type: GET_PRODUCTS, payload: allData.data})
+        return dispatch({ type: GET_PRODUCTS, payload: allData.data })
     }
 }
 
-export function getProductDetail(id){
-    return async function(dispatch) {
+export function getProductDetail(id) {
+    return async function (dispatch) {
         const productDetail = await axios.get(`/products/${id}`)
-        return dispatch({type: PRODUCT_DETAIL, payload: productDetail.data})
+        return dispatch({ type: PRODUCT_DETAIL, payload: productDetail.data })
     }
 }
 
-export function createProduct(payload){
-    return async function(){
+export function createProduct(payload) {
+    return async function () {
         let json = await axios.post('/products', payload)
         return json;
     }
 }
 
-export function updateProduct(payload){
-    return async function(dispatch){
+export function updateProduct(payload) {
+    return async function (dispatch) {
         const update = await axios.put(`/products`, payload)
-        return dispatch({type: PRODUCT_UPDATE, payload: update.payload})
+        return dispatch({ type: PRODUCT_UPDATE, payload: update.payload })
     }
 }
 
-export function getCategories(){
-    return async function(dispatch){
+export function getCategories() {
+    return async function (dispatch) {
         const allData = await axios.get('/category')
-        return dispatch({type: GET_CATEGORIES, payload: allData.payload})
+        return dispatch({ type: GET_CATEGORIES, payload: allData.payload })
     }
 }
 
-export function createCategories(payload){
-    return async function(){
+export function createCategories(payload) {
+    return async function () {
         let json = await axios.post('/category', payload)
+        return json;
+    }
+}
+
+export function getBrands() {
+    return async function (dispatch) {
+        const allData = await axios.get('/brand')
+        return dispatch({ type: GET_BRANDS, payload: allData.payload })
+    }
+}
+
+export function createBrands(payload) {
+    return async function () {
+        let json = await axios.post('/brand', payload)
         return json;
     }
 } 
