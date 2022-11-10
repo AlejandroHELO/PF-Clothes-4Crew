783f7e4ec4ff14b1621d2f0b22e82fa0368ae330
AlejandroHELO <alejohl0799@gmail.com>
2022-11-10 17:30:51 -0500

    detalles help us

--- client/src/components/HelpUsToImprove/HelpUsImprove.jsx
+++ client/src/components/HelpUsToImprove/HelpUsImprove.jsx
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux';
 import st from './HelpUsImprove.module.css'
 import { postComment } from '../../redux/actions.js'
 import Footer from '../Footer/Footer.jsx';
-
 import Overlay from 'react-bootstrap/Overlay';
 import Tooltip from 'react-bootstrap/Tooltip';
 
@@ -29,7 +28,7 @@ export default function HelpUsImprove() {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        if (e.target.name == 'update') {
+        if (e.target.name === 'update') {
             console.log('SOY EL MENSAJE', Message)
             dispatch(postComment(Message))
             setMessage({
@@ -41,38 +40,53 @@ export default function HelpUsImprove() {
     };
 
     return (
-        <div>
+        <div className={st.container}>
             <div className= {st.titlePage}>
-                <h2 className=" "> HELP US TO IMPROVE </h2>
+                <h2 className=""> ¡Leave us your comment and help us to improve! 🚀 </h2>
             </div>
 
             <div className={st.formCont}>
-
-                {/* <div className={st.float1}>
-                    <img src={image} className={st.img} alt="img" />
-                </div> */}
                 
-                <form classname={st.form} onSubmit={handleSubmit}>
-                    <div>
-                        <label>Name</label>
-                        <input type="text" name="name" onChange={handleChange} placeholder="Enter your name"> 
-                        </input>
+                <form classname={st.Form} onSubmit={handleSubmit}>
+                    
+                    <div className={st.InputForm}>
+                        <label className=' font-bold whitespace-pre-wrap'>Name</label>
+                        <input 
+                        type="text" 
+                        name="name" 
+                        onChange={handleChange} 
+                        placeholder="Enter your name"
+                        className={st.inputField} />
                     </div>
 
-                    <div>
-                        <label>Email</label>
-                        <input type={"email"} name="email" onChange={handleChange} placeholder="Enter your email"> 
-                        </input>
+                    <div className={st.InputForm}>
+                        <label className=' font-bold whitespace-pre-wrap'>Email</label>
+                        <input 
+                        type={"email"} 
+                        name="email" 
+                        onChange={handleChange} 
+                        placeholder="Enter your email"
+                        className={st.inputField} />
                     </div>
 
-                    <div>
-                        <label>Message</label>
-                        <textarea type="text" name="message" onChange={handleChange} placeholder="Write a message">
-                        </textarea>
+                    <div className={st.InputForm}>
+                        <label className=' font-bold whitespace-pre-wrap'>Message</label>
+                        <textarea 
+                        type="text" 
+                        name="message" 
+                        onChange={handleChange} 
+                        placeholder="Write a message"
+                        className={st.inputField} />
                     </div>
 
-                    <div>
-                        <button name='update' variant="primary" ref={target} type="submit" onClick={(e) => {
+                    <div className={st.InputButtonForm}>
+                        <button 
+                        className=' w-24 h-12 p-2 bg-slate-900 text-slate-50 rounded-lg flex justify-center items-center transition hover:bg-slate-50 hover:text-slate-900 hover:border-2 hover:border-slate-900'
+                        disabled = {Message.message === ""}
+                        name='update' 
+                        ref={target} 
+                        type="submit" 
+                        onClick={(e) => {
                             handleSubmit(e);
                             setShow(!show);
                         }}>
@@ -89,6 +103,10 @@ export default function HelpUsImprove() {
                     </Overlay>
 
                 </form>
+
+                <div className={st.imageDiv}>
+                    <img src="/images/newLogo.jpg" className={st.image} alt="img" />
+                </div>
             </div>
             <Footer />
         </div>
