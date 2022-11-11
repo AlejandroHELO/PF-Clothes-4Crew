6d149249ad1fadcf3857629532e709d65db1fce7
lucasmartindri <drilucasmartin@gmail.com>
2022-11-11 13:55:42 -0300

    reviews todavia

--- client/src/components/Product/ProductReview.jsx
+++ client/src/components/Product/ProductReview.jsx
@@ -1,14 +1,10 @@
 /*eslint-disable */
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { StarIcon } from '@heroicons/react/20/solid'
 import { getPReviews } from '../../redux/actions';
 import ReviewCard from './ReviewCard';
 import st from './ReviewCard.module.css';
 import { Link } from 'react-router-dom';
-import { Rating } from './CreateReviewsStyles';
-import { FaStar } from 'react-icons/fa';
-
 
 function ProductReview(product) {
     const dispatch = useDispatch()
@@ -16,27 +12,39 @@ function ProductReview(product) {
     // const product = useSelector((state) => state.details)
     // const reviews = useSelector(state => state.reviews)
     const reviews = useSelector(state => state.reviews)
-
+    
     React.useEffect(() => {
         dispatch(getPReviews())
     }, [dispatch])
 
     const reviewsFilter = reviews.filter(r => r.productId === product.id)
 
-    
     return (
         <div className={st.Container}>
             <div>
                 {reviewsFilter?.map((review) => {
-                    return(
-                        <Link to={`/cardReviews/${review._id}`}>
-                            <ReviewCard score={review.score} comment={review.comment.charAt(0).toUpperCase() + review.comment.slice(1)}/>  
+                    return (
+                        <Link to={`/cardReviews${review.id}`}>
+                            <ReviewCard score={review.score} comment={review.comment.charAt(0).toUpperCase() + review.comment.slice(1)}/>
                         </Link>
-                )})}
-            </div> 
+                    )
+                })}
+            </div>
         </div>
     )
 }
-
 export default ProductReview;
-/*eslint-enable */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
