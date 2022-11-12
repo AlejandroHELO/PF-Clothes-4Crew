d6cca528089c1d569c1ba802579ef888d85b0b8b
matalala <smitesotra@gmail.com>
2022-11-12 17:50:33 -0300

    configurando css  historial de compras

--- client/src/components/MyCheckout/MyCheckout.js
+++ client/src/components/MyCheckout/MyCheckout.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts, getProfile, GetPurchase } from "../../redux/actions";
 import ClipLoader from "react-spinners/ClipLoader";
 import { useNavigate } from "react-router-dom";
+import s from './MyCheckout.module.css'
 
 export default function MyCheckout(){
     const userDetail=useSelector(stata=>stata.userDetail)
@@ -11,6 +12,8 @@ export default function MyCheckout(){
     const dispatch=useDispatch()
     const nav=useNavigate()
     const {user,isAuthenticated,getAccessTokenSilently}=useAuth0()
+    const [desplegar,setdesplegar]=React.useState('')
+    const [mostrar,setmostrar]=React.useState({})
     React.useEffect(()=>{
         if(userDetail){
             dispatch(GetPurchase(userDetail._id))
@@ -23,38 +26,34 @@ export default function MyCheckout(){
     React.useEffect(()=>{
         dispatch(getProducts())
     })
-    let total=0;
+    
     const onbuton =(e)=>{
         nav('/products/reviews/'+e.target.id+'/'+userDetail._id)
     }
+    const onClickSelect=(b)=>{
+        if(b.paymentId===mostrar.paymentId){
+            setmostrar({})
+        }else{
+            setmostrar({...b,stateC:b.state})
+        }
+        
+    }
     return(
-        compras?compras?.map(b=>{
+      <div className={s.conten}>
+        <div className={s.contenlistcompras}>
+       { compras?compras?.map(b=>{
             return(
-                <div key={b.paymentId}>
-                    <h1>id orden: {b.paymentId}</h1>
-                    {b.products?.map((p)=>{
-                        total=total+p.price
-                        return(
-                            <div key={p.id}>
-                                <img src={p.image[0]} alt=''/>
-                                <h1>name: {p.name}</h1>
-                                <h1>brand: {p.brand}</h1>
-                                <h1>color: {p.color}</h1>
-                                <h1>price: {p.price}</h1>
-                               {b.state==='Finish'?
-                               <button
-                               id={p.id}
-                               onClick={onbuton}
-                               >Reviews</button>:null}
-                            </div>
-                        )
+                    <div className={s.itenlist} key={b.paymentId} onClick={()=>{onClickSelect(b)}}>
+                    <span>#{b.paymentId}</span>
+                    {b.state?<span>state: {b.state}</span>:null}
+                    <hr/>
+                    </div>                 
+                   
                     
-                    })}
-                        <h1>total: {total}</h1>
-                     {b.state?<h1>state: {b.state}</h1>:null}
-                </div>
             )
-        }):
+        })
+        
+        :
         <div
         style={{
           marginTop: "10rem",
@@ -67,6 +66,47 @@ export default function MyCheckout(){
               Please wait  
           </h1>
         <ClipLoader color="#ef8354" size={70} margin={10} />
+      </div>}
       </div>
+      <div className={s.contenD}>
+         {mostrar.products?mostrar.products?.map((p)=>{
+          return(
+              <div key={p.id} className={s.Dcompra}>
+                <div className={s.contenDcompra}>
+                  <img src={p.image[0]} alt=''/>
+                </div>
+                <div className={s.contenDcompra}>
+                    <div>
+                    <label>name: </label>
+                    <span>{p.name}</span>
+                    </div>
+                    <div>
+                    <label>brand: </label>
+                    <span>{p.brand}</span>
+                    </div>
+                    <div>
+                    <label>color: </label>
+                    <span>color: {p.color}</span>
+                    </div>
+                    <div>
+                    <label>price: </label>
+                    <span>price: {p.price}</span>
+                    </div>
+                </div>
+                <div className={s.contenDcompra}>
+                {mostrar.stateC==='Finish'?
+                    <button
+                    class=" my-10 mx-10 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    id={p.id}
+                    onClick={onbuton}
+                    >Reviews</button>:null}
+                </div>
+               </div>
+                )
+            
+            }):null}
+      </div>
+      </div>
+       
     )
 }
