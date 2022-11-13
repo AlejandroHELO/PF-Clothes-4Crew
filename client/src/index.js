2ebc529bbd3a42f6a8879a4e31dd25106779f191
matalala <smitesotra@gmail.com>
2022-11-13 06:54:35 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
     <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_CLIENT_ID}
-    redirectUri={window.location.origin}
+    audience={process.env.REACT_APP_AUDIENCE}
   >
         <Provider store={store}>
             <Router>
