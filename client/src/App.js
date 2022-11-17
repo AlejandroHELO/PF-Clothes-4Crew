893b17d09910b251eaa6aba48b19589f6bb31f9c
AlejandroHELO <alejohl0799@gmail.com>
2022-11-17 00:57:12 -0500

    Last commit :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { getProducts, getCart, getCurrentUser, getBrands, getColors, getCategories } from './redux/actions'
 import { useAuth0 } from "@auth0/auth0-react";
-// import { ConstructionOutlined } from '@mui/icons-materzial'
 import HomePage from './components/HomePage/HomePage'
 import AdminView from './components/Admin/AdminView'
-// import Loading from '';
 import HelpUsImprove from './components/HelpUsToImprove/HelpUsImprove'
-import SearchResults from './components/SearchResults/SearchResults'
-import Pago from './components/MercadoPago/MercadoPago'
 import Register from './components/login&register/register'
 import Navbar from './components/navbar/navbar'
 import Filters from './components/SearchResults/Filters'
@@ -21,20 +17,19 @@ import CCheckout from './components/CCheckout/CCheckout'
 import MyCheckout from './components/MyCheckout/MyCheckout'
 import UserProfile from './components/User/UserProfile';
 import AboutUs from './components/AboutUs/AboutUs'
+import ProductReview from './components/Product/ProductReview'
+import SearchResults from './components/SearchResults/SearchResults'
+import Pago from './components/MercadoPago/MercadoPago'
 import Footer from './components/Footer/Footer'
 // import './App.css';
-// import Loading from '';
-import ProductReview from './components/Product/ProductReview'
 
 
 export default function App() {
     const dispatch = useDispatch()
     const { isAuthenticated, getAccessTokenSilently, user } = useAuth0();
 
-
     useEffect(() => {
         if (isAuthenticated) {
-           
             dispatch(getCurrentUser(getAccessTokenSilently, user));
         }
     }, [isAuthenticated, getAccessTokenSilently, user]);
@@ -46,7 +41,6 @@ export default function App() {
         dispatch(getColors())
         dispatch(getBrands())
     }, [])
-
 
     return (
         <div className='w-full'>
@@ -79,10 +73,8 @@ export default function App() {
                 <Route path="*" element={<Navigate to="/" />} />
     
             </Routes>
-            {/* <Footer/> */}
             {/* <Pago id={'63615409b573f3a4a80dfc1f'}/> */}
         </div>
-
     )
 }
 
