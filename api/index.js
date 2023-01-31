046ec7d7b49efbfd26604fd846635fe8afc02b5e
victor cavallo <smitesotra@gmail.com>
2023-01-31 20:57:54 -0300

    commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const server = require('./src/app')
 const { PORT } = process.env
 const { dbConn } = require('./src/db') //conectamos la DB
-
+//comentario
 server.listen(PORT, () => {
     console.log(`Server rised and listening at port ${PORT}`)
 })
